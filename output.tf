8ba9b6a94eb92acf464acc730ffbb282b64aca6d
jykingston <jy.kingston@gmail.com>
2020-10-02 13:47:29 +1000

    Update outputs

--- output.tf
+++ output.tf
@@ -1,13 +1,13 @@
 # Insert output values here, if no outputs are defined delete this file
 
 output "web_acl_arn" {
-  value = aws_wafv2_web_acl.cloudfront_waf.arn
+  value = aws_wafv2_web_acl.cloudfront_waf[*].arn
 }
 
 output "web_acl_id" {
-  value = aws_wafv2_web_acl.cloudfront_waf.id
+  value = aws_wafv2_web_acl.cloudfront_waf[*].id
 }
 
 output "web_acl_name" {
-  value = aws_wafv2_web_acl.cloudfront_waf.name
+  value = aws_wafv2_web_acl.cloudfront_waf[*].name
 }
