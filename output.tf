7db8b4381c25f637cb163c2020506e54a497106e
jykingston <jy.kingston@gmail.com>
2020-10-07 17:10:51 +1100

    Update output to specific index

--- output.tf
+++ output.tf
@@ -1,13 +1,11 @@
-# Insert output values here, if no outputs are defined delete this file
-
 output "web_acl_arn" {
-  value = aws_wafv2_web_acl.cloudfront_waf[*].arn
+  value = aws_wafv2_web_acl.cloudfront_waf[0].arn
 }
 
 output "web_acl_id" {
-  value = aws_wafv2_web_acl.cloudfront_waf[*].id
+  value = aws_wafv2_web_acl.cloudfront_waf[0].id
 }
 
 output "web_acl_name" {
-  value = aws_wafv2_web_acl.cloudfront_waf[*].name
+  value = aws_wafv2_web_acl.cloudfront_waf[0].name
 }
